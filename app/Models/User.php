744c067247a980118e1b3ce7a75c0b63925a46c7
HoaiNamChu <chunam2606@gmail.com>
2024-10-08 20:51:24 +0700

    add cart table

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,4 +66,10 @@ class User extends Authenticatable
     {
         return $this->hasMany(ProductRating::class);
     }
+
+    public function cart()
+    {
+        return $this->hasOne(Cart::class);
+    }
+
 }
