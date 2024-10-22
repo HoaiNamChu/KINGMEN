88489387da1effb903df6cd2e872d13d4303ea53
NgocKhanhPhan <ngockhanhphan2003@gmail.com>
2024-10-22 10:48:06 +0700

    format controller

--- routes/web.php
+++ routes/web.php
@@ -25,56 +25,55 @@ use App\Http\Controllers\Client\AccountGoogleController;
 
 // viết các route client ở đây
 Route::prefix('/')->group(function () {
-    Route::get('/', function () {
-        return view('client.home.index');
-    })->name(name: '/');
+Route::get('/', function () {
+    return view('client.home.index');
+})->name(name: '/');
 
-    // view account
-    Route::get('/account', [AccountGoogleController::class, 'index'])->name('account.index');
-    
-    // login 
-    Route::get('/login', [AccountGoogleController::class, 'viewLogin'])->name('login');
-    Route::post('login', [AccountGoogleController::class, 'login'])->name('login.submit');
+// view account
+Route::get('/account', [AccountGoogleController::class, 'index'])->name('account.index');
 
-    // register
-    Route::get('/register', [AccountGoogleController::class, 'create'])->name('account.index');
-    Route::post('/register', [AccountGoogleController::class, 'store'])->name('store');
+// login 
+Route::get('/login', [AccountGoogleController::class, 'viewLogin'])->name('login');
+Route::post('login', [AccountGoogleController::class, 'login'])->name('login.submit');
 
-    // login by google
-    Route::get('auth/google', [AccountGoogleController::class, 'redirectToGoogle'])->name('login-by-google');
-    Route::get('auth/google/callback', [AccountGoogleController::class, 'handleGoogleCallback']);
+// register
+Route::get('/register', [AccountGoogleController::class, 'create'])->name('account.index');
+Route::post('/register', [AccountGoogleController::class, 'store'])->name('store');
 
-    // logout account
-    Route::get('/logout', [AccountGoogleController::class, 'logout'])->name('logout');
+// login by google
+Route::get('auth/google', [AccountGoogleController::class, 'redirectToGoogle'])->name('login-by-google');
+Route::get('auth/google/callback', [AccountGoogleController::class, 'handleGoogleCallback']);
 
-    // update billing address
-    Route::post('/update-billing-address', [AccountGoogleController::class, 'updateBillingAddress']);
+// logout account
+Route::get('/logout', [AccountGoogleController::class, 'logout'])->name('logout');
 
-    // forget password
-    Route::get('/forget-password', [AccountGoogleController::class, 'showForgetPasswordForm'])->name('forget.password.get');
+// update billing address
+Route::post('/update-billing-address', [AccountGoogleController::class, 'updateBillingAddress']);
 
-    Route::post('/forget-password', [AccountGoogleController::class, 'sendEmailForgetPasswordForm'])->name('forget.password.post'); 
+// forget password
+Route::get('/forget-password', [AccountGoogleController::class, 'showForgetPasswordForm'])->name('forget.password.get');
 
-    Route::get('reset-password/{token}', [AccountGoogleController::class, 'showResetPasswordForm'])->name('reset.password.get');
+Route::post('/forget-password', [AccountGoogleController::class, 'sendEmailForgetPasswordForm'])->name('forget.password.post'); 
 
-    Route::post('reset-password', [AccountGoogleController::class, 'submitResetPasswordForm'])->name('reset.password.post');
+Route::get('reset-password/{token}', [AccountGoogleController::class, 'showResetPasswordForm'])->name('reset.password.get');
+
+Route::post('reset-password', [AccountGoogleController::class, 'submitResetPasswordForm'])->name('reset.password.post');
 });
 
 // viết các route admin vào đây
 Route::prefix('/admin')
-    ->as('admin.')
-    ->group(function () {
-    Route::get('/', function () {
-        
- 
-        
-        return view('admin.dashboard.index');
-    })->middleware('checkPermission:Manage Users,Manage Roles,Manage Brands,Manage Permissions');
+->as('admin.')
+->group(function () {
+Route::get('/', function () {
+    
 
-    Route::resource('users', UserController::class)->middleware('checkPermission:Manage Users');
-    Route::resource('roles', RoleController::class)->middleware('checkPermission:Manage Roles');
-    Route::resource('brands', BrandController::class)->middleware('checkPermission:Manage Brands');
+    
+    return view('admin.dashboard.index');
+})->middleware('checkPermission:Manage Users,Manage Roles,Manage Brands,Manage Permissions');
 
-    Route::resource('permissions', PermissionController::class)->middleware('checkPermission:Manage Permissions');
+Route::resource('users', UserController::class)->middleware('checkPermission:Manage Users');
+Route::resource('roles', RoleController::class)->middleware('checkPermission:Manage Roles');
+Route::resource('brands', BrandController::class)->middleware('checkPermission:Manage Brands');
+Route::resource('permissions', PermissionController::class)->middleware('checkPermission:Manage Permissions');
 
 });
