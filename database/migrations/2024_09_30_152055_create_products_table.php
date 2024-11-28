836b304408aaca3efd1c979d230a4571497508a4
HoaiNamChu <chunam2606@gmail.com>
2024-11-28 07:50:02 +0700

    update database

--- database/migrations/2024_09_30_152055_create_products_table.php
+++ database/migrations/2024_09_30_152055_create_products_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(\App\Models\Brand::class)->nullable()->constrained();
+            $table->foreignIdFor(\App\Models\Category::class)->constrained();
+            $table->foreignIdFor(\App\Models\Brand::class)->constrained();
             $table->string('name');
             $table->string('slug')->unique();
             $table->string('sku')->unique();
@@ -24,8 +25,6 @@ return new class extends Migration
             $table->text('description')->nullable();
             $table->integer('quantity')->default(0);
             $table->boolean('is_active')->default(1);
-            $table->boolean('is_featured')->default(0);
-            $table->boolean('is_new')->default(0);
             $table->boolean('is_hot')->default(0);
             $table->boolean('is_sale')->default(0);
             $table->boolean('is_home')->default(0);
